281a76e8955012aca47879bd71d1add3fde52994
Park-seona <Brown484@naver.com>
2022-05-17 14:36:39 +0900

    0517작업시작

--- js/script.js
+++ js/script.js
@@ -25,20 +25,16 @@ $(document).ready(function(){
 
     //컬렉션 배너
     $(".contents .item1_wrapper .item1 img,.item1_wrapper .item1_box").mouseenter(function(){
-        $(".contents .collection_banner .collec_nav .item1_wrapper .item1_box").stop().animate({"bottom":"0"},280);
+        $(".contents .collection_banner .collec_nav .item1_wrapper .item1_box").stop().animate({"bottom":"15px"},280);
     });
     $(".contents .item1_wrapper .item1 img,.item1_wrapper .item1_box").mouseleave(function(){
-        $(".contents .collection_banner .collec_nav .item1_wrapper .item1_box").stop().animate({"bottom":"-60px"},280);
+        $(".contents .collection_banner .collec_nav .item1_wrapper .item1_box").stop().animate({"bottom":"-50px"},280);
     });
     $(".contents .item2_wrapper .item2 img,.item2_wrapper .item2_box").mouseenter(function(){
-        $(".contents .collection_banner .collec_nav .item2_wrapper .item2_box").stop().animate({"bottom":"0"},250);
+        $(".contents .collection_banner .collec_nav .item2_wrapper .item2_box").stop().animate({"bottom":"15px"},280);
     });
     $(".contents .item2_wrapper .item2 img,.item2_wrapper .item2_box").mouseleave(function(){
-        $(".contents .collection_banner .collec_nav .item2_wrapper .item2_box").stop().animate({"bottom":"-60px"},250);
+        $(".contents .collection_banner .collec_nav .item2_wrapper .item2_box").stop().animate({"bottom":"-50px"},280);
     });
 
-    //더 알아보기
-    // $(".contents .preview_banner .prev1 .item1_wrapper .item1_box").mouseenter(function(){
-    //     $(".contents .prev1 .item1_wrapper .item1_box").stop().animate({"background":"#000"},{"color":"#fff"});
-    // });
 });
