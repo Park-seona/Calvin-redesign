fea047f850f5dee78ce4671754c8f891025d4223
Park-seona <Brown484@naver.com>
2023-03-26 14:13:16 +0900

    20230326

--- js/script.js
+++ js/script.js
@@ -7,21 +7,6 @@ $(document).ready(function(){
     $("header .nav_box ul.main>li").mouseleave(function(){
         $(this).find("ul.sub_menu").stop().fadeOut();
     });
-
-    //이미지 슬라이드
-    $("ul.slide li").eq(0).siblings().hide();
-
-    var slideI=0;
-    setInterval(function(){
-
-        if(slideI<2) {
-            slideI++;
-        } else {
-            slideI=0;
-        }
-    $("ul.slide li").eq(slideI).siblings().fadeOut(500);
-    $("ul.slide li").eq(slideI).fadeIn(500);
-    },3000);
     
     //뉴배너 스와이퍼
     const swiper = new Swiper('.swiper', {
@@ -43,16 +28,16 @@ $(document).ready(function(){
 
     //컬렉션 배너
     $(".contents .item1_wrapper .item1 img,.item1_wrapper .item1_box").mouseenter(function(){
-        $(".contents .collection_banner .collec_nav .item1_wrapper .item1_box").stop().animate({"bottom":"15px"},280);
+        $(".contents .collection_banner .item1_wrapper .item1_box").stop().animate({"bottom":"15px"},280);
     });
     $(".contents .item1_wrapper .item1 img,.item1_wrapper .item1_box").mouseleave(function(){
-        $(".contents .collection_banner .collec_nav .item1_wrapper .item1_box").stop().animate({"bottom":"-50px"},280);
+        $(".contents .collection_banner .item1_wrapper .item1_box").stop().animate({"bottom":"-50px"},280);
     });
     $(".contents .item2_wrapper .item2 img,.item2_wrapper .item2_box").mouseenter(function(){
-        $(".contents .collection_banner .collec_nav .item2_wrapper .item2_box").stop().animate({"bottom":"15px"},280);
+        $(".contents .collection_banner .item2_wrapper .item2_box").stop().animate({"bottom":"15px"},280);
     });
     $(".contents .item2_wrapper .item2 img,.item2_wrapper .item2_box").mouseleave(function(){
-        $(".contents .collection_banner .collec_nav .item2_wrapper .item2_box").stop().animate({"bottom":"-50px"},280);
+        $(".contents .collection_banner .item2_wrapper .item2_box").stop().animate({"bottom":"-50px"},280);
     });
     
 });
